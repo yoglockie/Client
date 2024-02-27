f340140447153ecd1aa640b567d797d9488eda8f
yoglockie <yogilokesh916@gmail.com>
2024-02-27 22:05:04 +0530

    project start

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import HomePage from './Components/HomePage/HomePage';
+
 
 function App() {
   return (
     <div className="App">
-      <HomePage/>
+        Hello World      
     </div>
   );
 }
